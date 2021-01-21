389ff1945ae46897a4f79c2bd3ab5148b4c7f52b
palashbhusari <palashshankarbhusari@gmail.com>
2021-01-21 18:54:22 +0530

    connect and status check

---
+++ arm.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+from dronekit import connect, VehicleMode
+import time
+
+#Set up option parsing to get connection string
+import argparse  
+parser = argparse.ArgumentParser(description='Print out vehicle state information. Connects to SITL on local PC by default.')
+parser.add_argument('--connect', 
+                   help="vehicle connection target string. If not specified, SITL automatically started and used.")
+args = parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+baud_rate=921600
+
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
+# Connect to the Vehicle. 
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print("\nConnecting to vehicle on: %s" % connection_string)
+vehicle = connect(connection_string, baud=baud_rate, wait_ready=True)
+
+vehicle.wait_ready('autopilot_version')
+
+print(" Global Location: %s" % vehicle.location.global_frame)
+print(" Global Location (relative altitude): %s" % vehicle.location.global_relative_frame)
+print(" Local Location: %s" % vehicle.location.local_frame)
+print(" Attitude: %s" % vehicle.attitude)
+print(" Velocity: %s" % vehicle.velocity)
+print(" Is Armable?: %s" % vehicle.is_armable)
+print(" System status: %s" % vehicle.system_status.state)
+print(" Airspeed: %s" % vehicle.airspeed)    # settable
+print(" Mode: %s" % vehicle.mode.name)    # settable
